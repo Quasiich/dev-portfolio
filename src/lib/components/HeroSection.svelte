17f7683bb3b0a771e8d0babda0883a8104d49d71
Erik <48137656+Quasiich@users.noreply.github.com>
2025-03-18 22:00:20 +0100

    initalized project and created herosection

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,13 @@
+<section class="hero-section default-margin">
+  <h3>Hello iam Erik</h3>
+  <div class="underscore mb-m"></div>
+  <h1>I'm a upcoming web-developer</h1>
+  <h1 class="dark-grey">fueled by coffee</h1>
+  <button> lets talk </button>
+</section>
+
+<style>
+  .hero-section {
+    padding-top: 3%;
+  }
+</style>
