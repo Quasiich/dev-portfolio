3d2b25efbd3d8ffc3cd6b055d5bbdafa4e6fc5ea
Erik <48137656+Quasiich@users.noreply.github.com>
2025-03-18 22:20:09 +0100

    added <Button>

--- src/lib/components/Sections/HeroSection.svelte
+++ src/lib/components/Sections/HeroSection.svelte
@@ -1,9 +1,18 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$components";
+
+  function onclick() {
+    goto("/#contact-form");
+  }
+</script>
+
 <section class="hero-section default-margin">
   <h3>Hello iam Erik</h3>
   <div class="underscore mb-m"></div>
   <h1>I'm a upcoming web-developer</h1>
   <h1 class="dark-grey">fueled by coffee</h1>
-  <button> lets talk </button>
+  <Button {onclick}>Lets talk</Button>
 </section>
 
 <style>
