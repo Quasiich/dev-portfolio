55dd49323ee582d386a4abf6f5078de15ceefc18
Erik <48137656+Quasiich@users.noreply.github.com>
2025-03-23 22:31:47 +0100

    created interfaces for processed sanity data

--- src/lib/types/sanity.d.ts
+++ src/lib/types/sanity.d.ts
@@ -31,45 +31,60 @@ type SanityProject = {
     _type: "image";
   };
   date?: string;
-  content?: Array<
-    | {
-        children?: Array<{
-          marks?: Array<string>;
-          text?: string;
-          _type: "span";
-          _key: string;
-        }>;
-        style?:
-          | "normal"
-          | "h1"
-          | "h2"
-          | "h3"
-          | "h4"
-          | "h5"
-          | "h6"
-          | "blockquote";
-        listItem?: "bullet" | "number";
-        markDefs?: Array<{
-          href?: string;
-          _type: "link";
-          _key: string;
-        }>;
-        level?: number;
-        _type: "block";
-        _key: string;
-      }
-    | {
-        asset?: {
-          _ref: string;
-          _type: "reference";
-          _weak?: boolean;
-          [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
-        };
-        hotspot?: SanityImageHotspot;
-        crop?: SanityImageCrop;
-        _type: "image";
-        _key: string;
-      }
-  >;
+  content?: Array<RawTextContent | RawImageContent>;
   techStack?: Array<string>;
 };
+
+interface RawTextContent {
+  children: Array<{
+    marks?: Array<string>;
+    text: string;
+    _type: "span";
+    _key: string;
+  }>;
+  style: "normal" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote";
+  listItem?: "bullet" | "number";
+  markDefs?: Array<{
+    href?: string;
+    _type: "link";
+    _key: string;
+  }>;
+  level?: number;
+  _type: "block";
+  _key: string;
+}
+
+interface RawImageContent {
+  asset: {
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+  };
+  hotspot?: SanityImageHotspot;
+  crop?: SanityImageCrop;
+  _type: "image";
+  _key: string;
+}
+
+interface ProcessedProject {
+  name: string;
+  company: string;
+  dateAccomplished: string;
+  stack: string[];
+  projectImageUrl: string;
+  slug: string;
+  content: Array<ProcessedTextContent | ProcessedImageContent>;
+}
+
+interface ProcessedTextContent {
+  type: "text";
+  style: "normal" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote";
+  textToRender: string;
+}
+
+interface ProcessedImageContent {
+  type: "image";
+  url: string;
+  alt?: string;
+}
