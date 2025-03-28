c735eaaa97c34a3e9562db62a6ca05e1cfcfde2a
Erik <48137656+Quasiich@users.noreply.github.com>
2025-03-28 23:12:42 +0100

    added styling for contact section

--- src/lib/components/Sections/ContactSection.svelte
+++ src/lib/components/Sections/ContactSection.svelte
@@ -11,8 +11,8 @@
   <SectionHeadline sectionName="contact-form">Lets Talk</SectionHeadline>
   <div class="form-container default-margin mt-m">
     <form action="">
-      <input class="text-input mb-b" placeholder="Your Name" />
-      <input class="text-input mb-b" placeholder="Your Email" />
+      <input class="text-input mb-s" placeholder="Your Name" />
+      <input class="text-input mb-s" placeholder="Your Email" />
       <textarea name="" placeholder="Whats on your mind?"></textarea>
       <Button onclick={onSubmit}>Send it!</Button>
     </form>
@@ -31,3 +31,89 @@
     </div>
   </div>
 </section>
+
+<style>
+  section {
+    padding-bottom: 140px;
+  }
+
+  .form-container {
+    display: flex;
+    justify-content: space-between;
+
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      width: 45%;
+    }
+
+    form * {
+      font-size: 20px;
+      font-family: "Inter Tight", sans-serif;
+      font-weight: 500;
+      color: black;
+    }
+  }
+
+  .form-text {
+    width: 39%;
+  }
+
+  textarea,
+  input {
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.035);
+    border-radius: 8px;
+    padding: 4px 12px;
+    outline: none;
+    border: none;
+  }
+
+  input {
+    height: 48px;
+  }
+
+  textarea {
+    height: 120px;
+    margin-bottom: 40px;
+  }
+
+  textarea::placeholder,
+  input::placeholder {
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  .input-error {
+    background-color: rgba(rgba(214, 47, 47, 0.665), green, blue, alpha);
+  }
+
+  .input-error::placeholder {
+    color: white;
+  }
+
+  .spinner {
+    border: 4px solid rgba(0, 0, 0, 0.1);
+    border-left-color: black;
+    border-radius: 50%;
+    width: 16px;
+    height: 16px;
+    display: inline-block;
+    margin-right: 8px;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  .spinner-container {
+    display: flex;
+  }
+</style>
