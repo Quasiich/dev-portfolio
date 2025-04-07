fafcb47c46387115c37517493a7e558c270cf4cc
Erik <48137656+Quasiich@users.noreply.github.com>
2025-04-07 22:24:54 +0200

    added footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<footer class="footer">
+  <div class="footer-container">
+    <h3 class="semi-bold mb-xs">Erik Rettschlag</h3>
+    <div class="mb-l">
+      <a href="https://github.com/Quasiich" target="_blank" class="footer-link"
+        >GitHub</a
+      >
+      <a
+        href="https://www.linkedin.com/in/erik-rettschlag-200a0b240/"
+        target="_blank"
+        class="footer-link">LinkedIn</a
+      >
+    </div>
+    <p>© 2025 Erik Rettschlag. All rights reserved</p>
+  </div>
+</footer>
+
+<style>
+  .footer {
+    background-color: black;
+    color: white;
+    padding: 20px 20px;
+    text-align: center;
+  }
+
+  .footer-link {
+    margin: 0 10px;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Header } from "$components";
+  import { Header, Footer } from "$components";
   import "../app.css";
 
   let { children } = $props();
@@ -7,3 +7,4 @@
 
 <Header />
 {@render children()}
+<Footer />
